4ffc5f9fa1b27fb67f21a31e73009571fe399bf0
sumaiya500-r <111225010sumaiyasultana@gmail.com>
2026-08-25 22:47:32 +0600

    Add files via upload

---
+++ copearray.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n], b[n];
+
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        b[i] = a[i];
+    }
+
+    
+    printf("First array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+
+    
+    printf("Second array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", b[i]);
+    }
+
+    return 0;
+}
